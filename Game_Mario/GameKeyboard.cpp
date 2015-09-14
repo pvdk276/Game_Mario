b15370e5e239f7b66986d4d10de47c6b6e843684
cuongvd <voduycuong93@gmail.com>
2015-09-14 17:15:58 +0700

    Edit GameKeyboard

--- Game_Mario/GameKeyboard.cpp
+++ Game_Mario/GameKeyboard.cpp
@@ -1,13 +1,33 @@
 #include "GameKeyboard.h"
 
+GameKeyboard::GameKeyboard()
+{
+	di = NULL;
+	keyboard = NULL;
+}
+
+GameKeyboard::~GameKeyboard()
+{
+	destroy();
+}
+
 int GameKeyboard::Init(HINSTANCE hInstance, HWND hWnd)
 {
 	HRESULT result;
-	result = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (VOID**)&di, NULL);
-	if (result != DI_OK) return 0;
+	result = DirectInput8Create(GetModuleHandle(NULL), DIRECTINPUT_VERSION, IID_IDirectInput8, (VOID**)&di, NULL);
+	if (result != DI_OK)
+	{
+		OutputDebugString("[GameKeyboard.cpp] Error when create input object.");
+		return 0;
+	}
+
 
 	result = di->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
-	if (result != DI_OK) return 0;
+	if (result != DI_OK)
+	{
+		OutputDebugString("[GameKeyboard.cpp] Error when create input device.");
+		return 0;
+	}
 
 	result = keyboard->SetDataFormat(&c_dfDIKeyboard);
 	result = keyboard->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
@@ -26,4 +46,50 @@ int GameKeyboard::Init(HINSTANCE hInstance, HWND hWnd)
 	if (FAILED(result)) return 0;
 
 	return 1;
+}
+
+void GameKeyboard::destroy()
+{
+	if (keyboard != NULL)
+	{
+		keyboard->Unacquire();
+		keyboard->Release();
+		keyboard = NULL;
+	}
+
+	if (di != NULL)
+	{
+		di->Release();
+		di = NULL;
+	}
+}
+
+bool GameKeyboard::IsKeyDown(int key)
+{
+	if (keyStates[key] & 0x80)
+		return true;
+	return false;
+}
+
+bool GameKeyboard::IsKeyUp(int key)
+{
+	if (keyStates[key] & 0x80)
+		return false;
+	return true;
+}
+
+void GameKeyboard::PollKeyboard()
+{
+	keyboard->Poll();
+	if (keyboard->GetDeviceState(sizeof(keyStates), (LPVOID)&keyStates))
+	{
+		//keyboard device lost, try to re-acquire
+		keyboard->Acquire();
+		keyboard->GetDeviceState(sizeof(keyStates), (LPVOID)&keyStates);
+	}
+	else
+	{
+		keyboard->GetDeviceState(sizeof(keyStates), (LPVOID)&keyStates);
+	}
+
 }
--- Game_Mario/GameKeyboard.h
+++ Game_Mario/GameKeyboard.h
@@ -9,5 +9,24 @@ public:
 	BYTE keyStates[256];
 	DIDEVICEOBJECTDATA keyEvents[1024];
 
+	GameKeyboard();
+	~GameKeyboard();
+
+	//Khoi tao input object va input device
 	int Init(HINSTANCE hInstance, HWND hWnd);
+
+	//Huy input object va input device
+	void destroy();
+
+	//check neu key duoc nhan
+	//parameter example: DIK_LEFT
+	bool IsKeyDown(int);
+
+	//check neu key duoc tha ra
+	//parameter example: DIK_LEFT
+	bool IsKeyUp(int);
+
+	//get trang thai cua tat ca cac key
+	//duoc dat trong ham if() trong vong lap while(true), sau khi da khoi tao input device
+	void PollKeyboard();
 };
