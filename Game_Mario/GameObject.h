7bdd53b145fb9216eb7818f0ce883d26b66c90c9
thinh2906 <thinhnt296@gmail.com>
2015-11-03 13:56:34 +0700

    Revert "Revert "31/10 11:14""
    This reverts commit b83644f1e99b3610091adb08833b780793706eb8.

--- Game_Mario/GameObject.h
+++ Game_Mario/GameObject.h
@@ -1,45 +1,25 @@
 #pragma once
 
 #include "Box.h"
-#include<d3d9.h>
-#include<d3dx9.h>
-#include"Sprite.h"
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "Sprite.h"
 
 
 class CGameObject
 {
-//protected:
-//	D3DXVECTOR2 m_pos;
-//	D3DXVECTOR2 m_veloc;
-//	D3DXVECTOR2 m_accel;
-//	RECT m_bound;
-//	CSprite *m_sprite;
-//	//max
-//	D3DXVECTOR2 m_maxVelocity;
-//	D3DXVECTOR2 m_maxAccelemeter;
-//	//
-//	D3DXVECTOR2 StartPostion;
 public:
 	int direct;
 	int id;
 	int typeId;
-	/*float positionX;
-	float positionY;*/
+
 	D3DXVECTOR2 position;
 	float width;
 	float height;
-	/*float speedX;
-	float speedY;*/
 	D3DXVECTOR2 velocity;
 	D3DXVECTOR2 maxVelocity;
-	/*float maxSpeedX;
-	float maxSpeedY;*/
-	/*float accelX;
-	float accelY;*/
 	D3DXVECTOR2 accel;
 	D3DXVECTOR2 maxAccel;
-	/*float maxAccelX;
-	float maxAccelY;*/
 
 	CGameObject();
 	CGameObject(int id, int typeId, float positionX, float positionY, float width, float height);
---
+++ Game_Mario/HiddenObject.cpp
@@ -0,0 +1,1 @@
+#include "HiddenObject.h"
