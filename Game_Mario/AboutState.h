4bd3016b830fe804ad68e14bdd264f16d67fa0da
thanhtu2711 <tuphamhuynh@gmail.com>
2015-12-30 13:18:05 +0700

    aboutsate

---
+++ Game_Mario/AboutState.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "BaseGameState.h"
+#include "MenuState.h"
+#include "Sprite.h"
+#include "Camera.h"
+class CAboutState:public CBaseGameState
+{
+private:
+	CSprite* m_sprite;
+public:
+	CAboutState();
+	~CAboutState();
+	void Init();
+	void LoadResource();
+	void Update(float deltaTime);
+	void Render();
+	void End();
+};
+
