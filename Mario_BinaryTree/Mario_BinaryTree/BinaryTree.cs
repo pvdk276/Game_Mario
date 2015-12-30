82ac50a20e9dd8503feb2a19769282d987f14c17
thinh2906 <thinhnt296@gmail.com>
2015-12-30 09:57:13 +0700

    Mario_binarytree

---
+++ Mario_BinaryTree/Mario_BinaryTree/BinaryTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario_BinaryTree
+{
+    class BinaryTree
+    {
+        float widthOfCamera;
+
+        //private int id = 1;
+
+        public Node rootNode;
+
+        public List<string> chuoi = new List<string>();
+
+        public BinaryTree(float PositionX, float PositionY, float Width, float Height, float WidthCamera)
+        {
+            rootNode = new Node("00", PositionX, PositionY, Width, Height);
+            widthOfCamera = WidthCamera;
+        }
+
+        public void Build(Node node)
+        {
+            //check if node is large enough
+            if (node.width <= widthOfCamera) return;
+
+            //check if node has object
+            if (node.listObject.Count == 0) return;
+
+            node.nodeL = new Node(node.id + "00", node.positionX, node.positionY, node.width / 2, node.height);
+            node.nodeR = new Node(node.id + "01", node.positionX + node.width / 2, node.positionY, node.width / 2, node.height);
+            
+            foreach (GameObject gameObject in node.listObject)
+            {
+                if (node.nodeL.KiemTraThuoc(gameObject))
+                {
+                    node.nodeL.listObject.Add(gameObject);
+                }
+
+                if (node.nodeR.KiemTraThuoc(gameObject))
+                {
+                    node.nodeR.listObject.Add(gameObject);
+                }
+            }
+
+            node.listObject.Clear();
+
+            Build(node.nodeL);
+            Build(node.nodeR);
+        }
+
+        public void BinaryTreeText(Node node)
+        {
+            chuoi.Add(node.XuatChuoi());
+
+            if (node.nodeL != null) BinaryTreeText(node.nodeL);
+            if (node.nodeR != null) BinaryTreeText(node.nodeR);
+        }
+
+        public void GhiFileBinaryTree(string FilePath)
+        {
+            BinaryTreeText(rootNode);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(FilePath))
+            {
+                //string line = node.XuatChuoi();
+                foreach (string line in chuoi)
+                {
+                    file.WriteLine(line);
+                }
+
+            }
+        }
+    }
+}
---
+++ Mario_BinaryTree/Mario_BinaryTree/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mario_BinaryTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String filePath = txbFilePath.Text;
+                int numberOfColumns = Int32.Parse(txbNumberOfColumns.Text);
+                int numberOfRows = Int32.Parse(txbNumberOfRows.Text);
+
+                int[,] mt = FileUtils.GetInstance().LoadMatrix(filePath, numberOfRows, numberOfColumns);
+
+                BinaryTree tree = new BinaryTree(0, numberOfRows * 50, numberOfColumns * 50, numberOfRows * 50, 750);
+                String binaryTreeTextPath = @"C:\Users\ntthi\Downloads\Map_Mario\map1_BinaryTree.txt";
+                tree.rootNode.listObject = FileUtils.GetInstance().CreateObjectListFile(mt, numberOfRows, numberOfColumns);
+
+                using (System.IO.StreamWriter sw = System.IO.File.AppendText(@"C:\Users\ntthi\Downloads\Map_Mario\map1_ListObject.txt"))
+                {
+                    foreach (GameObject gameObject in tree.rootNode.listObject)
+                    {
+                        sw.WriteLine(gameObject.ToString());
+                    }
+                }
+
+                tree.Build(tree.rootNode);
+                tree.GhiFileBinaryTree(binaryTreeTextPath);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+    }
+}
